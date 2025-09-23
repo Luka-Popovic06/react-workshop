e0cce08d749d40999aa68025168e6594b37be63c
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-23 20:33:00 +0200

    add Counter component with increment and decrement functionality

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import React from "react";
+import logo from "./logo.svg";
+import Counter from "./Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to my counter</h2>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
