f98958607e2fb6d03796349b01ce1ec535f26713
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-24 19:26:07 +0200

    add Employee component with props for firstName, lastName and age

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 //import React from "react";
 import logo from "./logo.svg";
 import Counter from "./Counter";
+import Employee from "./Employee";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Welcome to my counter</h2>
-        <Counter />
+        <h1>Company Directory</h1>
+        <Employee firstName="Luka" lastName="P" age="12" />
+        <Employee firstName="Nikola" lastName="P" age="12" />
+        <Employee firstName="Zoran" lastName="P" age="12" />
+        <Employee firstName="Milica" lastName="P" age="12" />
       </header>
     </div>
   );
